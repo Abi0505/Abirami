3c44292ee8fa01d3db5104e6ddd4def29165f84f
Abi0505 <143527779+Abi0505@users.noreply.github.com>
2023-09-17 18:16:27 +0530

    Floyd's triangle.c

---
+++ Floyd's triangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,j,k=1;
+printf("enter n:");
+scanf(" %d",&n);
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=i;j++)
+{
+printf("%d ",k);
+k=k+1;
+}
+{
+printf("\n");
+}
+}
+getch();
+return 0;
+}
